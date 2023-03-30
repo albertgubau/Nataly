617d1b250e74408a8baa618891149a7ff45762f6
AlbertGubau <albert.gubau01@estudiant.upf.edu>
2023-03-30 17:18:16 +0200

    Removed unused pages

--- new_page.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import *
-
-
-class Stft_model(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        uic.loadUi('new_page.ui', self)
